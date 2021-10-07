e563bc10b76388d17ecbcc250d00c43deea7f0c0
Semburg <semburg.sb@gmail.com>
2021-10-07 10:17:34 +0200

    header fast fertig - styling the navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log('test');
+
+// NAVIGATION 
+
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+const navbar = document.getElementById('nav_groups');
+
+openBtn.addEventListener('click', ()=>{
+    navbar.classList.add('show');
+})
+
+closeBtn.addEventListener('click', ()=>{
+    navbar.classList.remove('show');
+})
